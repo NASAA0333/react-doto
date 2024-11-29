5c37254aefd601a4d66586adbbbf2df03f72d2cc
NASAA0333 <nasanbuyan0333@gmail.com>
2024-11-29 17:54:44 +0800

    11-29school

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,71 @@
 "use client";
-
-import { useState } from "react";
+import "./todo-style.css";
+import "./todo-box";
 export default function Home() {
-  const [index, setIndex] = useState(0);
-  
   return (
-    <>
-      <button type="button" onClick={() => setIndex(index + 1)} className="">
-        +
-      </button>
-      <button type="button" onClick={() => setIndex(index - 1)} className="">
-        -
-      </button>
-      <h1>{index}</h1>
-      <button onClick={}>add</button>
-    </>
-    
+    <div>
+      <div class="main-inside">
+        <div class="header">
+          <button onclick="addTodo()" class="add-task-button">
+            Add task
+          </button>
+        </div>
+
+        <div class="all-black-box">
+          <div class="black-box">
+            <div class="box-header">
+              <div class="white-sircle"></div>
+              <h1 class="title">To do</h1>
+              <div id="todoCounter" class="meter"></div>
+            </div>
+            <div id="todos" class="todos"></div>
+          </div>
+
+          <div class="black-box">
+            <div class="box-header">
+              <div class="yellow-sircle"></div>
+              <h1 class="title">In progress</h1>
+              <div id="inProgressCounter" class="meter"></div>
+            </div>
+            <div id="inprogress" class="todos"></div>
+          </div>
+
+          <div class="black-box">
+            <div class="box-header">
+              <div class="green-sircle"></div>
+              <h1 class="title">Done</h1>
+              <div id="doneCounter" class="meter"></div>
+            </div>
+            <div id="done" class="todos"></div>
+          </div>
+
+          <div class="black-box">
+            <div class="box-header">
+              <div class="red-sircle"></div>
+              <h1 class="title">Blocked</h1>
+              <div id="blockedCounter" class="meter"></div>
+            </div>
+            <div id="blocked" class="todos"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="modal" id="modal">
+        <div class="modal-content">
+          <h2>Enter task</h2>
+          <input type="text" id="taskName" class="add-text" />
+
+          <select id="task-status" value="todo">
+            <option value="todos">Todo</option>
+            <option value="inprogress">in progress</option>
+            <option value="done">done</option>
+            <option value="blocked">Blocked</option>
+          </select>
+          <button onclick="saveTodo()" class="add-task-button-2">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/app/todo-box.js
+++ src/app/todo-box.js
@@ -1,3 +1,43 @@
-export default function box() {
-  return <div>12345</div>;
+"use client";
+import "./style.css";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <button
+        onClick={openModal}
+        style={{
+          position: "absolute",
+          border: "1px white solid",
+          width: "100px",
+          height: "40px",
+          marginTop: "60px",
+        }}
+      >
+        Add
+      </button>
+
+      {isOpen && (
+        <div className="modal">
+          <div style={{ marginTop: "40px" }}>Modal</div>
+          <input
+            type="text"
+            style={{
+              height: "45px",
+              width: "300px",
+              backgroundColor: "transparent",
+              border: "1px solid white",
+            }}
+          ></input>
+        </div>
+      )}
+      <div className="todo"></div>
+    </div>
+  );
 }
