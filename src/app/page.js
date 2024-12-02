7f50aadd8c2cec7581b0c4aa0baf1508102cd17a
NASAA0333 <nasanbuyan0333@gmail.com>
2024-12-02 19:09:10 +0800

    12-2school

--- src/app/page.js
+++ src/app/page.js
@@ -1,71 +1,38 @@
 "use client";
-import "./todo-style.css";
-import "./todo-box";
+
+import "./style.css";
+import Image from "next/image";
+import { useState } from "react";
+
+let todos = [];
+
+let numbers = "#" + todos[i].status;
+let taskNumber = 0;
+for (let i = 0; i < todos.length; i++) {
+  if (numbers === "#Number") {
+    taskNumber++;
+  }
+}
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
   return (
-    <div>
-      <div class="main-inside">
-        <div class="header">
-          <button onclick="addTodo()" class="add-task-button">
-            Add task
-          </button>
+    <div id="header">
+      <button onClick={openModal} id="add-button">
+        Add
+      </button>
+
+      {isOpen && (
+        <div>
+          <div>Modal</div>
+          <div id="Number"></div>
+          <input type="text" id="imput"></input>
+          <button id="">save</button>
         </div>
-
-        <div class="all-black-box">
-          <div class="black-box">
-            <div class="box-header">
-              <div class="white-sircle"></div>
-              <h1 class="title">To do</h1>
-              <div id="todoCounter" class="meter"></div>
-            </div>
-            <div id="todos" class="todos"></div>
-          </div>
-
-          <div class="black-box">
-            <div class="box-header">
-              <div class="yellow-sircle"></div>
-              <h1 class="title">In progress</h1>
-              <div id="inProgressCounter" class="meter"></div>
-            </div>
-            <div id="inprogress" class="todos"></div>
-          </div>
-
-          <div class="black-box">
-            <div class="box-header">
-              <div class="green-sircle"></div>
-              <h1 class="title">Done</h1>
-              <div id="doneCounter" class="meter"></div>
-            </div>
-            <div id="done" class="todos"></div>
-          </div>
-
-          <div class="black-box">
-            <div class="box-header">
-              <div class="red-sircle"></div>
-              <h1 class="title">Blocked</h1>
-              <div id="blockedCounter" class="meter"></div>
-            </div>
-            <div id="blocked" class="todos"></div>
-          </div>
-        </div>
-      </div>
-
-      <div class="modal" id="modal">
-        <div class="modal-content">
-          <h2>Enter task</h2>
-          <input type="text" id="taskName" class="add-text" />
-
-          <select id="task-status" value="todo">
-            <option value="todos">Todo</option>
-            <option value="inprogress">in progress</option>
-            <option value="done">done</option>
-            <option value="blocked">Blocked</option>
-          </select>
-          <button onclick="saveTodo()" class="add-task-button-2">
-            Submit
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
